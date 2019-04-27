ef5e1931bc9b5a168670ebbd76e90abd552a71ec
jacynkaa <jacynkaa@bitbucket.org>
2019-04-27 17:02:16 +0200

    Added scaling to Menus.

--- core/src/main/java/testcraft/MainMenuScreen.java
+++ core/src/main/java/testcraft/MainMenuScreen.java
@@ -6,7 +6,6 @@ import com.badlogic.gdx.InputMultiplexer;
 import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.*;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
@@ -19,12 +18,12 @@ import org.mini2Dx.core.graphics.Graphics;
 import org.mini2Dx.core.game.*;
 import org.mini2Dx.core.graphics.HeadlessGraphics;
 import org.mini2Dx.core.graphics.LibGdxGraphics;
+import org.mini2Dx.core.graphics.TextureRegion;
 import org.mini2Dx.core.screen.BasicGameScreen;
 import org.mini2Dx.core.screen.ScreenManager;
 import org.mini2Dx.core.screen.transition.FadeInTransition;
 import org.mini2Dx.core.screen.transition.FadeOutTransition;
-
-
+import static com.badlogic.gdx.graphics.Texture.TextureWrap.Repeat;
 import static testcraft.TestCraftGame.multiplexer;
 
 
@@ -35,6 +34,17 @@ public class    MainMenuScreen extends BasicGameScreen {
     private Stage stage;
     public   boolean startGame=false;
     public Texture bg;
+    TextureRegion bgRegion;
+
+    static final float WIDTH = 1280;
+    static final float HEIGHT = 720;
+
+    float SCREEN_WIDTH = 1280;
+    float SCREEN_HEIGHT = 720;
+    float scale = 1f;
+    float transX = 0f;
+    float transY = 0f;
+
 
 
 
@@ -60,10 +70,15 @@ public class    MainMenuScreen extends BasicGameScreen {
             e.printStackTrace();
         }
 
-        stage = gc.createStage(new ScreenViewport());
+        stage = gc.createStage(new ExtendViewport(WIDTH,HEIGHT));
 
-       bg = new Texture(Gdx.files.absolute("craftacular/raw/dirt.png"));
-       bg.setWrap(Texture.TextureWrap.Repeat, Texture.TextureWrap.Repeat); // <- it will be background in the future
+        bg = new Texture(Gdx.files.absolute("craftacular/raw/dirt.png"));
+        bg.setWrap(Texture.TextureWrap.Repeat, Texture.TextureWrap.Repeat); // <- it will be background in the future
+
+
+        bgRegion = new TextureRegion(bg);
+        bgRegion.setRegion(0, 0, 1280, 720);
+
 
 
         addButton(500,400,new ClickListener(){
@@ -107,7 +122,20 @@ public class    MainMenuScreen extends BasicGameScreen {
     @Override
     public void render(GameContainer gc, Graphics g) {
 
+        SCREEN_HEIGHT = gc.getHeight();
+        SCREEN_WIDTH = gc.getWidth();
+        float scaleX = SCREEN_WIDTH/WIDTH;
+        float scaleY = SCREEN_HEIGHT/HEIGHT;
+        scale = Math.min(scaleX, scaleY);
+        transX = -Math.max(0, (SCREEN_WIDTH-scale*WIDTH)/2)/scale;
+        transY = -Math.max(0, (SCREEN_HEIGHT-scale*HEIGHT)/2)/scale;
+        g.drawRect(-1, -1, SCREEN_WIDTH+2, SCREEN_HEIGHT+2);
+        g.setScale(scale, scale);
+        g.translate(transX, transY);
+        g.drawTextureRegion(bgRegion,0,0);//,WIDTH*scale,HEIGHT*scale);
+        stage.getViewport().update(gc.getWidth(), gc.getHeight(), false);
         g.drawStage(stage);
+
     }
     @Override
     public int getId(){
--- core/src/main/java/testcraft/MenuScreen.java
+++ core/src/main/java/testcraft/MenuScreen.java
@@ -3,6 +3,8 @@ package testcraft;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.InputMultiplexer;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
@@ -10,13 +12,18 @@ import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.utils.viewport.ExtendViewport;
+import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
 import org.mini2Dx.core.graphics.Graphics;
 import org.mini2Dx.core.game.*;
+import org.mini2Dx.core.graphics.TextureRegion;
 import org.mini2Dx.core.screen.BasicGameScreen;
 import org.mini2Dx.core.screen.ScreenManager;
 import org.mini2Dx.core.screen.transition.NullTransition;
 
+import static com.badlogic.gdx.graphics.Texture.TextureWrap.Repeat;
 import static testcraft.TestCraftGame.multiplexer;
 
 
@@ -24,11 +31,28 @@ import static testcraft.TestCraftGame.multiplexer;
 public class    MenuScreen extends BasicGameScreen {
     public static int ID = 3;
 
+
+
+
+
     private static Skin skin;
     private Stage stage;
-    private  Stage background;
     public   boolean startGame=false;
     public boolean toMainMenu=false;
+    public Texture bg;
+    TextureRegion bgRegion;
+
+
+
+    static final float WIDTH = 1280;
+    static final float HEIGHT = 720;
+
+    float SCREEN_WIDTH = 1280;
+    float SCREEN_HEIGHT = 720;
+    float scale = 1f;
+    float transX = 0f;
+    float transY = 0f;
+
 
 
     public  void addButton (float x, float y, ClickListener clickListener, String text)
@@ -51,7 +75,17 @@ public class    MenuScreen extends BasicGameScreen {
         catch (Exception e) {
             e.printStackTrace();
         }
-        stage = gc.createStage(new ScreenViewport());
+        stage = gc.createStage(new ExtendViewport(WIDTH,HEIGHT));
+
+        bg = new Texture(Gdx.files.absolute("craftacular/raw/dirt.png"));
+        bg.setWrap(Texture.TextureWrap.Repeat, Texture.TextureWrap.Repeat); // <- it will be background in the future
+
+
+
+
+        bgRegion = new TextureRegion(bg);
+        bgRegion.setRegion(0, 0, 1280, 720);
+
 
         addButton(500,400,new ClickListener(){
             @Override
@@ -75,6 +109,7 @@ public class    MenuScreen extends BasicGameScreen {
                 Gdx.app.exit();
             }
         }, "QUIT");
+       // stage.setDebugAll(true);
 
     }
 
@@ -98,7 +133,6 @@ public class    MenuScreen extends BasicGameScreen {
     public void update(GameContainer gc,  ScreenManager screenManager, float delta) {
         if(!multiplexer.getProcessors().contains(stage,false))
             multiplexer.addProcessor(stage);
-        stage.getViewport().update(width, height, true);
 
         if(startGame || Gdx.input.isKeyPressed(Input.Keys.ESCAPE))
             toGame(screenManager);
@@ -113,8 +147,25 @@ public class    MenuScreen extends BasicGameScreen {
 
     @Override
     public void render(GameContainer gc, Graphics g) {
+
+
+        SCREEN_HEIGHT = gc.getHeight();
+        SCREEN_WIDTH = gc.getWidth();
+        float scaleX = SCREEN_WIDTH/WIDTH;
+        float scaleY = SCREEN_HEIGHT/HEIGHT;
+        scale = Math.min(scaleX, scaleY);
+        transX = -Math.max(0, (SCREEN_WIDTH-scale*WIDTH)/2)/scale;
+        transY = -Math.max(0, (SCREEN_HEIGHT-scale*HEIGHT)/2)/scale;
+        g.drawRect(-1, -1, SCREEN_WIDTH+2, SCREEN_HEIGHT+2);
+        g.setScale(scale, scale);
+        g.translate(transX, transY);
+        g.drawTextureRegion(bgRegion,0,0);//,WIDTH*scale,HEIGHT*scale);
+        stage.getViewport().update(gc.getWidth(), gc.getHeight(), false);
         g.drawStage(stage);
+
+
     }
+
     @Override
     public int getId(){
         return ID;
