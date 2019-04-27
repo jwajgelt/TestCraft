fd013c022818334450fdb3a792eb9181414186c2
jacynkaa <jacynkaa@bitbucket.org>
2019-04-27 23:22:16 +0200

    Minor fix to save/load.

--- core/src/main/java/testcraft/World.java
+++ core/src/main/java/testcraft/World.java
@@ -152,7 +152,7 @@ class World {
     }
 
     void saveToDisk(){
-        //System.out.println("Saving the world!");
+        System.out.println("Saving the world!");
         for(WorldChunk chunk : chunks){
             try {
                 ObjectOutputStream stream = new ObjectOutputStream(new FileOutputStream("."+File.separator+worldName+File.separator+worldName+"_"+chunk.chunkPosX+"_"+chunk.chunkPosY));
