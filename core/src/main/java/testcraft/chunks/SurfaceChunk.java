70c0a5cf65df44762f55376baf5b1eca17c333f5
juliusz <jwajgelt@gmail.com>
2019-05-06 22:14:51 +0200

    Some minor tweaks to SurfaceChunk generation

--- core/src/main/java/testcraft/chunks/SurfaceChunk.java
+++ core/src/main/java/testcraft/chunks/SurfaceChunk.java
@@ -2,10 +2,7 @@ package testcraft.chunks;
 
 import testcraft.ChunkLoader;
 import testcraft.WorldChunk;
-import testcraft.blocks.CoalBlock;
-import testcraft.blocks.CobblestoneBlock;
-import testcraft.blocks.DirtBlock;
-import testcraft.blocks.GrassDirtBlock;
+import testcraft.blocks.*;
 
 import java.io.Serializable;
 import java.util.Random;
@@ -15,41 +12,46 @@ import static java.lang.Math.min;
 
 public class SurfaceChunk extends WorldChunk implements Serializable {
 
-    protected Random randy = new Random();
-
-    final int[] surfaceLevel = new int[CHUNK_SIZE];
+    private final int[] surfaceLevel = new int[CHUNK_SIZE];
 
     public SurfaceChunk(int xPos, int yPos, ChunkLoader chunkLoader){
         super(xPos, yPos);
         surfaceLevel[0] = -1;
+        //if the chunk to the left is already generated, continue at the same height
+        Random randy = new Random();
         if(chunkLoader.checkChunkAvailable(xPos - CHUNK_SIZE, yPos)){
             WorldChunk leftChunk = chunkLoader.getChunk(xPos - CHUNK_SIZE, yPos);
             if(leftChunk instanceof SurfaceChunk){
-                surfaceLevel[0] = ((SurfaceChunk)leftChunk).surfaceLevel[CHUNK_SIZE-1];
+                surfaceLevel[0] = max(1, min(CHUNK_SIZE-2, ((SurfaceChunk)leftChunk).surfaceLevel[CHUNK_SIZE-1] + (randy.nextInt(4)+1)/2-1));
                 for(int i = 1; i < CHUNK_SIZE; i++){
-                    surfaceLevel[i] = max(0, min(CHUNK_SIZE-2, surfaceLevel[i-1] + (randy.nextInt(4)+1)/2-1));
+                    surfaceLevel[i] = max(1, min(CHUNK_SIZE-2, surfaceLevel[i-1] + (randy.nextInt(4)+1)/2-1));
                 }
             }
         }
+        //same if the chunk to the right is already generated
         if(chunkLoader.checkChunkAvailable(xPos + CHUNK_SIZE, yPos)){
             WorldChunk rightChunk = chunkLoader.getChunk(xPos + CHUNK_SIZE, yPos);
             if(rightChunk instanceof SurfaceChunk){
-                surfaceLevel[CHUNK_SIZE-1] = ((SurfaceChunk)rightChunk).surfaceLevel[0];
+                surfaceLevel[CHUNK_SIZE-1] = max(1, min(CHUNK_SIZE-2, ((SurfaceChunk)rightChunk).surfaceLevel[0] + (randy.nextInt(4)+1)/2-1));
                 for(int i = CHUNK_SIZE-2; i >= 0; i--){
-                    surfaceLevel[i] = max(0, min(CHUNK_SIZE-2, surfaceLevel[i+1] + (randy.nextInt(4)+1)/2-1));
+                    surfaceLevel[i] = max(1, min(CHUNK_SIZE-2, surfaceLevel[i+1] + (randy.nextInt(4)+1)/2-1));
                 }
             }
         }
+        //if neither are, generate whatever
         if(surfaceLevel[0] == -1){
-            surfaceLevel[0] = randy.nextInt(CHUNK_SIZE/16);
+            surfaceLevel[0] = randy.nextInt(CHUNK_SIZE/16)+1;
             for(int i = 1; i < CHUNK_SIZE; i++){
-                surfaceLevel[i] = max(0, min(CHUNK_SIZE-2, surfaceLevel[i-1] + (randy.nextInt(4)+1)/2-1));
+                surfaceLevel[i] = max(1, min(CHUNK_SIZE-2, surfaceLevel[i-1] + (randy.nextInt(4)+1)/2-1));
             }
         }
         for(int i = 0; i < CHUNK_SIZE; i++){
+            //top level should be grassy dirt
             blocks[i][surfaceLevel[i]] = new GrassDirtBlock();
+            //throw some grass blocks on top
+            if(randy.nextInt(2) == 0) blocks[i][surfaceLevel[i]-1] = new GrassBlock();
             for(int j = surfaceLevel[i]+1; j < CHUNK_SIZE; j++){
-                blocks[i][j] = (j < surfaceLevel[i]+2) ? new DirtBlock() : (randy.nextBoolean()) ? (randy.nextBoolean() ? new DirtBlock() : new CoalBlock()) : randy.nextBoolean() ? new DirtBlock() : new CobblestoneBlock(randy.nextInt(10));;
+                blocks[i][j] = (j < surfaceLevel[i]+2) ? new DirtBlock() : (randy.nextBoolean()) ? (randy.nextBoolean() ? new DirtBlock() : new CoalBlock()) : randy.nextBoolean() ? new DirtBlock() : new CobblestoneBlock(randy.nextInt(10));
             }
         }
     }
