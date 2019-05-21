561b2ac39775efe5b5717ce838acac05ea8c287c
jacynkaa <jacynkaa@bitbucket.org>
2019-05-21 22:11:56 +0200

    Fixed minimizing screen errors.

--- desktop/src/testcraft/desktop/DesktopLauncher.java
+++ desktop/src/testcraft/desktop/DesktopLauncher.java
@@ -1,17 +1,31 @@
 package testcraft.desktop;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.LifecycleListener;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.utils.SnapshotArray;
+import org.lwjgl.Sys;
 import org.mini2Dx.desktop.DesktopMini2DxConfig;
 
 import com.badlogic.gdx.backends.lwjgl.DesktopMini2DxGame;
 
 import testcraft.TestCraftGame;
 
+import java.lang.reflect.Field;
+
 public class DesktopLauncher {
-    public static void main (String[] arg) {
+    public static void main (String[] arg) throws Exception {
+
+
         DesktopMini2DxConfig config = new DesktopMini2DxConfig(TestCraftGame.GAME_IDENTIFIER);
+        config.pauseWhenMinimized=false;
+        config.pauseWhenBackground=false;
         config.vSyncEnabled = true;
         config.width = 1280;
         config.height = 720;
-        new DesktopMini2DxGame(new TestCraftGame(), config);
+        config.useGL30=true; //jak cos to mozna wylaczyc
+        DesktopMini2DxGame desktopMini2DxGame = new DesktopMini2DxGame(new TestCraftGame(), config);
     }
+
 }
